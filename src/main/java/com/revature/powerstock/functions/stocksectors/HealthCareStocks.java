a9cb41a952880052beb364b6bd3f1015ad35bcd9
nappingrn <49659790+nappingrn@users.noreply.github.com>
2019-06-02 19:45:50 -0500

    Update HealthCareStocks.java

--- src/main/java/com/revature/powerstock/functions/stocksectors/HealthCareStocks.java
+++ src/main/java/com/revature/powerstock/functions/stocksectors/HealthCareStocks.java
@@ -1,3 +1,5 @@
+package com.revature.powerstock.functions.stocksectors;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -30,61 +32,6 @@ public class HealthCareStocks {
 				
 				// URL
 					String techURL = "https://cloud.iexapis.com/stable/stock/market/collection/sector?collectionName=Health%20Care&token=sk_77ef8dc8d96c43f7ae4624d7ea5b228e";	
-				
-				// URL DATA -> String
-					String techDataString = Stock.sendGet(techURL);
-				
-				// Technology Sector Data
-					JSONArray techData = (JSONArray) stockParser.parse(techDataString);
-				
-				
-				// While Loop Iteration
-					int stockNumber = 0;
-					
-				// Gather 10 stockNames from TechSector
-					while(stockNumber<10)
-					{
-						String stockName;
-						String stockSymbol;
-						try {
-							JSONObject stock = (JSONObject) techData.get(stockNumber);
-							stockSymbol = (String) stock.get("symbol");
-							stockName = (String) stock.get("companyName");
-							
-							stockSymbols.add(stockSymbol);
-							stockNames.add(stockName);
-							
-							sectorData.put(stockSymbol, stockName);
-							stockNumber++;		
-						
-						} catch (Exception e) {
-							stockNumber++;
-						}	
-					}
-					
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			sector.add("<h1> This is a list of HealthCare tickers</h1>\n");
-			sector.add(" "+stockSymbols.get(0) + " - " + stockNames.get(0) +" <br>");
-			sector.add(" "+stockSymbols.get(1) + " - " + stockNames.get(1) +" <br>");
-			sector.add(" "+stockSymbols.get(2) + " - " + stockNames.get(2) +" <br>");
-			sector.add(" "+stockSymbols.get(3) + " - " + stockNames.get(3) +" <br>");
-			sector.add(" "+stockSymbols.get(4) + " - " + stockNames.get(4) +" <br>");
-			sector.add(" "+stockSymbols.get(5) + " - " + stockNames.get(5) +" <br>");
-			sector.add(" "+stockSymbols.get(6) + " - " + stockNames.get(6) +" <br>");
-			sector.add(" "+stockSymbols.get(7) + " - " + stockNames.get(7) +" <br>");
-			sector.add(" "+stockSymbols.get(8) + " - " + stockNames.get(8) +" <br>");
-			sector.add(" "+stockSymbols.get(9) + " - " + stockNames.get(9) +" <br>");
-
-			
-			return sectorData;
-
-		}
-}
-";	
 				
 				// URL DATA -> String
 					String techDataString = Stock.sendGet(techURL);
